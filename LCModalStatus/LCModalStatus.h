490201158b753bb036dc23db4edb35fd3d50858d
Luana Chen <luanachen@gmail.com>
2018-04-20 14:33:38 -0300

    Initial Commit

---
+++ LCModalStatus/LCModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  LCModalStatus.h
+//  LCModalStatus
+//
+//  Created by Luana on 20/04/18.
+//  Copyright © 2018 lccj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LCModalStatus.
+FOUNDATION_EXPORT double LCModalStatusVersionNumber;
+
+//! Project version string for LCModalStatus.
+FOUNDATION_EXPORT const unsigned char LCModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LCModalStatus/PublicHeader.h>
+
+
